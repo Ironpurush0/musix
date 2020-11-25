ec91a0bbeaaa86a126d2d48805aacebe8abc0bf6
Ironpurush0 <ridamsaxena90.com>
2020-11-26 02:19:12 +0530

    .

--- backend/musix/api/models.py
+++ backend/musix/api/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Room(models.Model):
-    code = models.CharField(max_length=8, default="", unique=true)
-    host = models.CharField(max_length=80, unique=true)
-    guest_can_pause = models.BooleanField(null=false, default=false)
-    votes_to_skip = models.IntegerField(null=false, default=1)
-    created_at = models.DateTimeField(auto_now_add=true)
+    code = models.CharField(max_length=8, default="", unique=True)
+    host = models.CharField(max_length=80, unique=True)
+    guest_can_pause = models.BooleanField(null=False, default=False)
+    votes_to_skip = models.IntegerField(null=False, default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
 
 
